e9e348c56ea8090d1ca528fe1e1c73172f051e0f
Luke Garrison <lgarriso@nd.edu>
2016-12-12 03:07:51 -0500

    Adds hw4, part3 (improvements that reach 90% on test data)

--- hw4/README.md
+++ hw4/README.md
@@ -12,14 +12,23 @@ You can also type ```python3 ./get_probabilistic_cfg.py -h``` for more help on r
 
 ## Parsing Sentences
 
-First, the training data must be given to viterbi_cky.py which will use get_probabilistic_cfg.py to generate a probabilistic_cfg using the training data. Then, each sentence from dev.strings is read in and the best parse is found for each sentence using the viterbi-cky algorithm. The parsed sentence is output to the file dev.parses. This output should then be run through the postprocess script and saved as dev.parses.post. Then, the evalb.py script can be used to compare dev.parses.post to dev.trees for precesion, recall, and an F1 score.
+First, the training data must be given to viterbi_cky.py which will use get_probabilistic_cfg.py to generate a probabilistic_cfg using the training data. Then, each sentence from [dev/test].strings is read in and the best parse is found for each sentence using the viterbi-cky algorithm. The parsed sentence is output to the file [dev/test].parses. This output should then be run through the postprocess script and saved as [dev/test].parses.post. Then, the evalb.py script can be used to compare dev.parses.post to dev.trees for precesion, recall, and an F1 score.
+
+Three modifications are possible:
+
+* ``` --vertical_markovization ``` applies vertical markovization
+* ``` --unknowns ``` increased the probability of rules producing unknowns
+* ``` --add_delta ``` increases the count of each rule, disproportionately increases the probability of rules with lower counts
 
 
 #### How To Run
 
-	python3 viterbi_cky.py train.trees.pre.unk hw4_data/dev.strings
-	python3 hw4_utilities/postprocess.py dev.parses > dev.parses.post
-	python3 hw4_utilities/evalb.py dev.parses.post hw4_data/dev.trees
+	python3 viterbi_cky.py train.trees.pre.unk hw4_data/[dev/test].strings [options]
+	python3 hw4_utilities/postprocess.py [dev/test].parses > [dev/test].parses.post
+	python3 hw4_utilities/evalb.py [dev/test].parses.post hw4_data/[dev/test].trees
+
+
+You can also type ```python3 ./viterbi_cky.py -h``` for more help on running the program
 
 
 ## Requirements
--- hw4/viterbi_cky.py
+++ hw4/viterbi_cky.py
@@ -2,6 +2,7 @@
 # Hw4, part 2
 # Takes a sentence and grammar as input and outputs the highest probability parse
 
+import re
 import sys
 import math
 import argparse
@@ -14,12 +15,9 @@ from hw4_utilities.tree import Tree
 import get_probabilistic_cfg as get_cfg
 
 
-def parse_lines(trees_file, sentences_file, probabilistic_cfg, probabilistic_cfg_vm):
-	parse_tree_outfile = 'dev.parses'
-
-	outfile = open(parse_tree_outfile, "w")
-
-	print('Output of parser on first five lines, along with their log10 probabilities:')
+# try parsing the line using vertical markovization. if that fails, use standard parsing
+def parse_lines_vm(trees_file, sentences_file, parse_tree_out_filename, probabilistic_cfg, probabilistic_cfg_vm):
+	outfile = open(parse_tree_out_filename, "w")
 
 	with open(sentences_file) as f:
 		for line_index, line in enumerate(f):
@@ -58,7 +56,36 @@ def parse_lines(trees_file, sentences_file, probabilistic_cfg, probabilistic_cfg
 
 
 	outfile.close()
-	print("parser output located in:", parse_tree_outfile)
+	print("parser output located in:", parse_tree_out_filename)
+
+
+# use viterbi-cky algorithm to parse each sentence
+def parse_lines(trees_file, sentences_file, parse_tree_out_filename, probabilistic_cfg):
+	outfile = open(parse_tree_out_filename, "w")
+
+	with open(sentences_file) as f:
+		for line_index, line in enumerate(f):
+			line = line.strip()
+			tree_list = list()
+			tree_string = str()
+
+			final_back_pointer, back_pointers, best = viterbi_cky(line, probabilistic_cfg)
+
+			# if both vertical markovizatin and standard were possible, use the parse with the larger probability
+			if final_back_pointer[2] is not None:
+				get_tree_list(final_back_pointer, back_pointers, line.split(' '), tree_list)
+				tree_string = ''.join(tree_list)
+			else:
+				# unable to parse the line
+				pass
+
+			tree_string += '\n'
+
+			outfile.write(tree_string)
+
+
+	outfile.close()
+	print("parser output located in:", parse_tree_out_filename)
 
 
 def get_tree_list(back_pointer, back_pointers, sentence_list, tree_list):
@@ -156,20 +183,59 @@ def viterbi_cky(sentence, probabilistic_cfg):
 	return (final_cell_indices[0], final_cell_indices[1], max_prob_label), back_pointers, best
 
 
+def unknown_smoothing(rule_counts, delta):
+	for left_side in rule_counts:
+		rule_counts[left_side]['<unk>'] += delta
+
+def add_delta_smoothing(rule_counts, delta):
+	for left_side in rule_counts:
+		for right_side in rule_counts[left_side]:
+			rule_counts[left_side][right_side] += delta
+
+
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Outputs the highest probability parse given a probabilistic CFG and sentence')
 	parser.add_argument("trees_file", type=str, help="File containing trees with CFG rules")
 	parser.add_argument("sentences_file", type=str, help="File containing input sentences to parse")
+	parser.add_argument('--vertical_markovization', action='store_true', help='Try to use vertical markovization for better parses')
+	parser.add_argument('--unknowns', action='store_true', help='Put more weight on rules that produce <unk>')
+	parser.add_argument('--add_delta', action='store_true', help='Apply add-delta smoothing to the model')
 
 	args = parser.parse_args()
 
 	trees_file = args.trees_file
 	sentences_file = args.sentences_file
+	use_vertical_markovization = args.vertical_markovization
+	increase_unknowns = args.unknowns
+	add_delta = args.add_delta
 
+	is_test_file_re = re.compile('test\.strings$')
+	if re.search(is_test_file_re, sentences_file):
+		out_filename = 'test.parses'
+	else:
+		out_filename = 'dev.parses'
+
+	# get cfg rule counts
 	rule_counts = get_cfg.count_rules(trees_file)
-	probabilistic_cfg = get_cfg.get_probabilistic_cfg(rule_counts)
-
 	rule_counts_vertical_markovization = get_cfg.count_rules_vertical_markovization(trees_file)
+
+	if increase_unknowns:
+		# only use this if vertical markovization is not being used
+		if not use_vertical_markovization:
+			unknown_smoothing(rule_counts, 15)
+		unknown_smoothing(rule_counts_vertical_markovization, 15)
+
+	if add_delta:
+		# only use this if vertical markovization is not being used
+		if not use_vertical_markovization:
+			add_delta_smoothing(rule_counts, 10)
+		add_delta_smoothing(rule_counts_vertical_markovization, 10)
+
+	# get cfg rule probabilities
+	probabilistic_cfg = get_cfg.get_probabilistic_cfg(rule_counts)
 	probabilistic_cfg_vm = get_cfg.get_probabilistic_cfg(rule_counts_vertical_markovization)
 
-	parse_lines(trees_file, sentences_file, probabilistic_cfg, probabilistic_cfg_vm)
+	if use_vertical_markovization:
+		parse_lines_vm(trees_file, sentences_file, out_filename, probabilistic_cfg, probabilistic_cfg_vm)
+	else:
+		parse_lines(trees_file, sentences_file, out_filename, probabilistic_cfg)
